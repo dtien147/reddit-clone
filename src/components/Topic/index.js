b550a350874e27533bb9c0a029a0f814101cd179
Tien Duong <dtien147@gmail.com>
2019-05-17 09:56:02 +0700

    Update topic layout

--- src/components/Topic/index.js
+++ src/components/Topic/index.js
@@ -3,26 +3,24 @@ import PropTypes from 'prop-types';
 import './styles.scss';
 
 const Topic = React.memo(function Topic({
+  id,
   content,
   votes,
-  author,
-  createdAt,
   upvote,
   downvote
 }) {
   return (
     <div className="topic">
       <div className="vote-wrapper">
-        <button onClick={upvote} className="upvote-btn">
-          <i className="fas fa-arrow-up" />
+        <button onClick={upvote} className="upvote-btn" id={id}>
+          <i className="fas fa-arrow-up" id={id}/>
         </button>
         <div className="votes">{votes}</div>
-        <button onClick={downvote} className="downvote-btn">
-          <i className="fas fa-arrow-down" />
+        <button onClick={downvote} className="downvote-btn" id={id}>
+          <i className="fas fa-arrow-down" id={id} />
         </button>
       </div>
       <div className="content-wrapper">
-        <div className="header">Posted by {author} {createdAt}</div>
         <div className="content">{content}</div>
       </div>
     </div>
@@ -30,10 +28,9 @@ const Topic = React.memo(function Topic({
 });
 
 Topic.propTypes = {
+  id: PropTypes.string.isRequired,
   content: PropTypes.string.isRequired,
   votes: PropTypes.number.isRequired,
-  author: PropTypes.string.isRequired,
-  createdAt: PropTypes.string.isRequired,
   upvote: PropTypes.func.isRequired,
   downvote: PropTypes.func.isRequired,
 };
