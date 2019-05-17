f18011e0c80871fe5f586cd5c667725028f16f81
Tien Duong <dtien147@gmail.com>
2019-05-17 09:55:30 +0700

    Update apis - Return a list of top 20 topics (sorted by upvotes, descending) - Check topics content when creating - Fix downvote, upvote issue

--- server/index.js
+++ server/index.js
@@ -1,31 +1,37 @@
 const express = require('express');
 const uuid = require('uuid');
 const cors = require('cors');
+const orderBy = require('lodash/orderBy');
 
 const port = process.env.PORT || 5000;
 const app = express();
+const SORT_NAME = 'votes';
+const SORT_ORDER = 'desc';
+const MAX_CONTENT_LENGTh = 255;
+
+const ERROR_MESSAGE = {
+  NOT_EMPTY: 'Content must not be empty.',
+  EXCEED_MAX_LENGTH: 'Content must not exceed 255 characters.',
+};
+
 const topics = [
   {
     id: '1234',
-    author: 'test 1',
     content: 'test a',
     votes: 1,
   },
   {
     id: '12345',
-    author: 'test 2',
     content: 'test b',
     votes: 2,
   },
   {
     id: '12346',
-    author: 'test 3',
     content: 'test c',
     votes: 3,
   },
   {
     id: '12347',
-    author: 'test 4',
     content: 'test d',
     votes: 4,
   }
@@ -38,29 +44,57 @@ app.get('/api', function(req, res) {
   res.send('API OK');
 });
 
+/**
+ * API for getting list of topic
+ * Return a list of top 20 topics (sorted by upvotes, descending)
+ */
 app.get('/api/topics', function(req, res){
-  res.send(topics);
+  res.send(orderBy(topics, [SORT_NAME], [SORT_ORDER]).slice(0, 20));
 });
 
+/**
+ * API for creating topics
+ * Receive topic's content from user
+ * Then generate topic's id and add it to topic list
+ */
 app.post('/api/topics', function(req, res) {
   const newTopic = req.body;
-  newTopic.id = uuid.v4();
-  topics.push(newTopic);
-  res.send(newTopic);
+  if (!newTopic.content) {
+    res.status(400);
+    res.send(ERROR_MESSAGE.NOT_EMPTY);
+  } else if (newTopic.content.length > MAX_CONTENT_LENGTh) {
+    res.status(400);
+    res.send(ERROR_MESSAGE.EXCEED_MAX_LENGTH);
+  } else {
+    newTopic.id = uuid.v4();
+    newTopic.votes = 0;
+    topics.push(newTopic);
+    res.send(newTopic);
+  }
 });
 
+/**
+ * API for upvoting a topic
+ * Receive topic's id from user
+ * Then increase its vote by 1
+ */
 app.post('/api/topics/upvote', function(req, res) {
   const topicId = req.body.id;
   const topic = topics.find(item => item.id === topicId);
-  topic.upvote += 1;
-  res.send('OK');
+  topic.votes += 1;
+  res.send(topic);
 });
 
+/**
+ * API for downvoting a topic
+ * Receive topic's id from user
+ * Then decrease its vote by 1
+ */
 app.post('/api/topics/downvote', function(req, res) {
   const topicId = req.body.id;
   const topic = topics.find(item => item.id === topicId);
-  topic.upvote -= 1;
-  res.send('OK');
+  topic.votes -= 1;
+  res.send(topic);
 });
 
 app.listen(port, () => {
